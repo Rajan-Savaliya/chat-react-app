203a1708322e657291d8dca5676208b6abf8eac0
rajan savaliya <rajanpatel99094@gmail.com>
2020-06-01 16:34:57 +0530

    pwa start it

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Join from './components/Join/Join';
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const App = () => {
+const App = () => { 
   return (
     <Router>
       <Route path="/" exact component={Join} />
